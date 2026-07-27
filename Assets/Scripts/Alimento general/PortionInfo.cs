e1a50f29ef5f9dce9973e8201a2249d304cf75d9
Jona <Aznar_Wolfpelt@outlook.com>
2026-07-27 11:11:54 -0500

    Ajustes

--- Assets/Scripts/Alimento general/PortionInfo.cs
+++ Assets/Scripts/Alimento general/PortionInfo.cs
@@ -6,11 +6,26 @@ public class PortionInfo : MonoBehaviour
 
     private FoodManager manager;
     private FoodData food;
+    public FloatingFoodLabel floatingLabel;
+
+    void Awake()
+    {
+        floatingLabel = GetComponentInChildren<FloatingFoodLabel>();
+    }
 
     public void Initialize(FoodManager foodManager, FoodData foodData)
     {
         manager = foodManager;
         food = foodData;
+
+        if (floatingLabel != null)
+        {
+            floatingLabel.Setup(
+                food.foodName,
+                food.portionDisplayNames[portionIndex],
+                food.portionWeights[portionIndex]
+            );
+        }
     }
 
     public void ShowInfo()
